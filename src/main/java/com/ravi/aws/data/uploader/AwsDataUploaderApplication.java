8e7346a7525338763be342972f33a98e88814696
ravisharma <ravi.neo02@gmail.com>
2023-08-03 00:13:51 -0400

    initial commit

---
+++ src/main/java/com/ravi/aws/data/uploader/AwsDataUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.aws.data.uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDataUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDataUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/aws/data/uploader/AwsDataUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.aws.data.uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDataUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
